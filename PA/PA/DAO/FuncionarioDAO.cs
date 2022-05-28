01aaa6d05cc42250591689b4350df734c00f43e8
arturrpereira <artur.fernando@protonmail.com>
2022-05-28 19:38:16 -0300

    19:38

---
+++ PA/PA/DAO/FuncionarioDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PA.Model;
+using Npgsql;
+using PA.db;
+
+namespace PA.DAO
+{
+    public class FuncionarioDAO
+    {
+        public void insert(Funcionario model)
+        {
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "INSERT INTO Funcionario (nome_funcionario,rg_funcionario,cpf_funcionario,endereco_funcionario,numero_endereco_funcionario,bairro_funcionario,dataadmissao_funcionario,datanasc_funcionario,genero_funcionario,telefone_funcionario,fk_id_cargo) " +
+                                  "VALUES (@nome_funcionario,@rg_funcionario,@cpf_funcionario,@endereco_funcionario,@numero_endereco_funcionario,@bairro_funcionario,@dataadmissao_funcionario,@datanasc_funcionario,@genero_funcionario,@telefone_funcionario,@fk_id_cargo)";
+
+            command.Parameters.AddWithValue("@nome_funcionario", model.nome_funcionario);
+            command.Parameters.AddWithValue("@rg_funcionario", model.rg_funcionario);
+            command.Parameters.AddWithValue("@cpf_funcionario", model.cpf_funcionario);
+            command.Parameters.AddWithValue("@endereco_funcionario", model.endereco_funcionario);
+            command.Parameters.AddWithValue("@numero_endereco_funcionario", model.numero_endereco_funcionario);
+            command.Parameters.AddWithValue("@bairro_funcionario", model.bairro_funcionario);
+            command.Parameters.AddWithValue("@dataadmissao_funcionario", model.dataAdmissao_funcionario);
+            command.Parameters.AddWithValue("@datanasc_funcionario", model.dataNasc_funcionario);
+            command.Parameters.AddWithValue("@genero_funcionario", model.genero_funcionario);
+            command.Parameters.AddWithValue("@telefone_funcionario", model.telefone_funcionario);
+            command.Parameters.AddWithValue("@fk_id_cargo", model.fk_id_cargo);
+
+            ConnectionDB.CRUD(command);
+        }
+    }
+}
---
+++ PA/PA/View/index.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace PA.View
+{
+    partial class index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(index));
+            this.menuVert = new System.Windows.Forms.Panel();
+            this.btnCadFunc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BoxLogo = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnCadPet = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Principal = new System.Windows.Forms.Panel();
+            this.BarraServico = new System.Windows.Forms.Panel();
+            this.btnMaximizar = new System.Windows.Forms.PictureBox();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnSair = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuVert.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BoxLogo)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.BarraServico.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSair)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuVert
+            // 
+            this.menuVert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(77)))));
+            this.menuVert.Controls.Add(this.btnCadFunc);
+            this.menuVert.Controls.Add(this.panel1);
+            this.menuVert.Controls.Add(this.button1);
+            this.menuVert.Controls.Add(this.BoxLogo);
+            this.menuVert.Controls.Add(this.panel5);
+            this.menuVert.Controls.Add(this.btnCadPet);
+            this.menuVert.Controls.Add(this.panel3);
+            this.menuVert.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuVert.Location = new System.Drawing.Point(0, 0);
+            this.menuVert.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.menuVert.Name = "menuVert";
+            this.menuVert.Size = new System.Drawing.Size(219, 489);
+            this.menuVert.TabIndex = 0;
+            // 
+            // btnCadFunc
+            // 
+            this.btnCadFunc.BackColor = System.Drawing.Color.Transparent;
+            this.btnCadFunc.FlatAppearance.BorderSize = 0;
+            this.btnCadFunc.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.btnCadFunc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCadFunc.Image = ((System.Drawing.Image)(resources.GetObject("btnCadFunc.Image")));
+            this.btnCadFunc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadFunc.Location = new System.Drawing.Point(4, 212);
+            this.btnCadFunc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCadFunc.Name = "btnCadFunc";
+            this.btnCadFunc.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.btnCadFunc.Size = new System.Drawing.Size(214, 48);
+            this.btnCadFunc.TabIndex = 4;
+            this.btnCadFunc.Text = "  Cadastro de Funcionarios";
+            this.btnCadFunc.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCadFunc.UseVisualStyleBackColor = false;
+            this.btnCadFunc.Click += new System.EventHandler(this.btnCadFunc_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 212);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(4, 48);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(-116, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(4, 50);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(4, 152);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(214, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "  Cadastro de Cliente";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // BoxLogo
+            // 
+            this.BoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("BoxLogo.Image")));
+            this.BoxLogo.Location = new System.Drawing.Point(0, 0);
+            this.BoxLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BoxLogo.Name = "BoxLogo";
+            this.BoxLogo.Size = new System.Drawing.Size(219, 60);
+            this.BoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BoxLogo.TabIndex = 0;
+            this.BoxLogo.TabStop = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Location = new System.Drawing.Point(0, 152);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(4, 48);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(-116, 0);
+            this.panel6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(4, 50);
+            this.panel6.TabIndex = 3;
+            // 
+            // btnCadPet
+            // 
+            this.btnCadPet.BackColor = System.Drawing.Color.Transparent;
+            this.btnCadPet.FlatAppearance.BorderSize = 0;
+            this.btnCadPet.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.btnCadPet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadPet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCadPet.Image = ((System.Drawing.Image)(resources.GetObject("btnCadPet.Image")));
+            this.btnCadPet.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCadPet.Location = new System.Drawing.Point(4, 89);
+            this.btnCadPet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCadPet.Name = "btnCadPet";
+            this.btnCadPet.Padding = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.btnCadPet.Size = new System.Drawing.Size(214, 48);
+            this.btnCadPet.TabIndex = 0;
+            this.btnCadPet.Text = "  Cadastro de Cliente";
+            this.btnCadPet.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCadPet.UseVisualStyleBackColor = false;
+            this.btnCadPet.Click += new System.EventHandler(this.btnCadPet_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(0, 89);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(4, 48);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(-116, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(4, 50);
+            this.panel4.TabIndex = 3;
+            // 
+            // Principal
+            // 
+            this.Principal.BackColor = System.Drawing.Color.Teal;
+            this.Principal.Location = new System.Drawing.Point(219, 58);
+            this.Principal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Principal.Name = "Principal";
+            this.Principal.Size = new System.Drawing.Size(903, 431);
+            this.Principal.TabIndex = 0;
+            // 
+            // BarraServico
+            // 
+            this.BarraServico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(168)))), ((int)(((byte)(11)))));
+            this.BarraServico.Controls.Add(this.btnMaximizar);
+            this.BarraServico.Controls.Add(this.btnMinimizar);
+            this.BarraServico.Controls.Add(this.btnSair);
+            this.BarraServico.Controls.Add(this.pictureBox1);
+            this.BarraServico.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarraServico.Location = new System.Drawing.Point(219, 0);
+            this.BarraServico.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BarraServico.Name = "BarraServico";
+            this.BarraServico.Size = new System.Drawing.Size(903, 60);
+            this.BarraServico.TabIndex = 1;
+            this.BarraServico.Paint += new System.Windows.Forms.PaintEventHandler(this.BarraServico_Paint);
+            // 
+            // btnMaximizar
+            // 
+            this.btnMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMaximizar.Image")));
+            this.btnMaximizar.Location = new System.Drawing.Point(816, 17);
+            this.btnMaximizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMaximizar.Name = "btnMaximizar";
+            this.btnMaximizar.Size = new System.Drawing.Size(26, 22);
+            this.btnMaximizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMaximizar.TabIndex = 3;
+            this.btnMaximizar.TabStop = false;
+            this.btnMaximizar.Click += new System.EventHandler(this.btnMaximizar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(777, 17);
+            this.btnMinimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(26, 22);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 2;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.Location = new System.Drawing.Point(856, 17);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(26, 22);
+            this.btnSair.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnSair.TabIndex = 1;
+            this.btnSair.TabStop = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(856, 22);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 489);
+            this.Controls.Add(this.Principal);
+            this.Controls.Add(this.BarraServico);
+            this.Controls.Add(this.menuVert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "index";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "index";
+            this.Load += new System.EventHandler(this.index_Load);
+            this.menuVert.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BoxLogo)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.BarraServico.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSair)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel menuVert;
+        private PictureBox BoxLogo;
+        private Button btnCadPet;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel Principal;
+        private Panel BarraServico;
+        private Button button1;
+        private Panel panel5;
+        private Panel panel6;
+        private Button btnCadFunc;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private PictureBox btnMaximizar;
+        private PictureBox btnMinimizar;
+        private PictureBox btnSair;
+    }
+}
